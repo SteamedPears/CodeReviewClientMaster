0283bbd3df57232d29b479a373bec5b41fd89de4
Simon Pratt <me@SimonDavidPratt.com>
2012-06-19 14:22:08 -0500

    Code cleanup and minor bug fixes

--- view.js
+++ view.js
@@ -1,30 +1,58 @@
 /******************************************************************************
-* view.js                                                                     *
-* Copyright 2012, Simon Pratt                                                 *
-******************************************************************************/
+ * view.js                                                                     *
+ * Copyright 2012, Simon Pratt                                                 *
+ ******************************************************************************/
 (function() {
     var comments_div,
     selected_line_start = -1,
     selected_line_end = -1,
     highlightColour = 'LightSalmon',
     comments = {};
-    
-    function printCode(n,line) {
-	var toPrint = '<tr>';
-	toPrint += '<td><input name="line_start" type="radio" value="'+n+'"></td>';
-	toPrint += '<td><input name="line_end" type="radio" value="'+n+'"></td>';
-	toPrint += '<td>'+n+'</td>';
-	toPrint += '<td id="line'+n+'" class="pre"></td>';
-	toPrint += '<td id="comment'+n+'"></td>';
-	toPrint += '</tr>';
-	$('#code_table').append(toPrint);
-	$('td#line'+n).text(line);
+
+/******************************************************************************
+* Utility Functions                                                           *
+******************************************************************************/
+
+    function modulo(n,m) {
+	while(n < 0)
+	    n += m;
+	return n%m;
     }
 
-    function printComments(text) {
-	comments_div.append(text);
+    function reportError(error) {
+	console.log(error); // good enough for now
     }
-    
+
+/******************************************************************************
+* Highlighting
+******************************************************************************/
+
+    function highlightLines(start,end,colour) {
+	while(start <= end) {
+	    $('#line' + start).css('background',colour);
+	    ++start;
+	}
+    }
+
+    function clearHighlighting() {
+	highlightLines(selected_line_start,selected_line_end,'');
+	selected_line_start = -1;
+	selected_line_end = -1;
+    }
+
+    function highlightComment(comment) {
+	clearHighlighting();
+	highlightLines(comment.line_start,
+		       comment.line_end,
+		       highlightColour);
+	selected_line_start = comment.line_start;
+	selected_line_end = comment.line_end;
+    }
+
+/******************************************************************************
+* Data retrieval                                                              *
+******************************************************************************/
+
     function getCode(id,success_fn,error_fn) {
 	$.get('do/code',{id:id},success_fn);
     }
@@ -32,20 +60,10 @@
     function getComments(id,success_fn,error_fn) {
 	$.get('do/comments',{code_id:id},success_fn);
     }
-
-    function writeCode(code) {
-	if(code === null) return;
-	var lines = code.text.split('\n');
-	for(var i in lines) {
-	    printCode(Number(i)+1,lines[i]+'\n');
-	}
-	$('input#code_id').val(code.id);
-	getComments(code.id,writeComments,writeCommentsError);
-    }
-
-    function writeCodeError() {
-	printCode("An error occured while retrieving code");
-    }
+    
+/******************************************************************************
+* Comment Display                                                             *
+******************************************************************************/
 
     function buildCommentStructure(comments_ob) {
 	var comments_list = comments_ob.comments;
@@ -58,15 +76,16 @@
 	}
     }
 
-    function writeComments(comments_ob) {
-	buildCommentStructure(comments_ob);
-	closeComments();
-    }
-
     function clearComments() {
 	for(var i in comments) {
 	    $('#comment'+i).text('');
 	}
+    }
+
+    function displayComment(dom_ob,comment_ob) {
+	dom_ob.text('');
+	dom_ob.append($('<h3>').text(comment_ob.user));
+	dom_ob.append($('<span>').text(comment_ob.text));
     }
 
     function closeComments() {
@@ -77,10 +96,9 @@
 	}
     }
 
-    function modulo(n,m) {
-	while(n < 0)
-	    n += m;
-	return n%m;
+    function writeComments(comments_ob) {
+	buildCommentStructure(comments_ob);
+	closeComments();
     }
 
     function openComment(n) {
@@ -88,19 +106,24 @@
 	var i = -1;
 	var num = comments[n].length;
 	var ob = $('<div>');
-
+	
 	// actual comment
 	var comment = $('<div>');
 	comment.attr('id','selected_comment');
 	comment.css('position','absolute');
-	var top = $('#line'+(1+Number(n))).position().top;
+	
+	var top = Number($('#line'+n).position().top);
+	var height = $('#line'+n).css('height');
+	height = height.substring(0,height.indexOf('px'));
+	top += Number(height);
 	comment.css('top',top);
 	var left = $('#comment'+n).position().left;
 	comment.css('left',left);
 	comment.appendTo($('body'));
 	
 	// previous button
-	var prev = $('<span class="button">');
+	var prev = $('<span>');
+	prev.attr('class','button');
 	prev.append('Previous');
 	prev.click(function() {
 	    i = modulo(i-1,num);
@@ -111,7 +134,8 @@
 	ob.append(' ');
 	
 	// next button
-	var next = $('<span class="button">');
+	var next = $('<span>');
+	next.attr('class','button');
 	next.append('Next');
 	next.click(function() {
 	    i = modulo(i+1,num);
@@ -123,7 +147,8 @@
 	ob.append(' ');
 
 	// close button
-	var close = $('<span class="button">');
+	var close = $('<span>');
+	close.attr('class','button');
 	close.append('X');
 	close.click(function() {
 	    comment.remove();
@@ -135,53 +160,60 @@
 	$('#comment'+n).append(ob);
     }
 
-    function displayComment(dom_ob,comment_ob) {
-	dom_ob.text('');
-	dom_ob.append($('<h3>').text(comment_ob.user));
-	dom_ob.append($('<span>').text(comment_ob.text));
-    }
-
-    function clearHighlighting() {
-	highlightLines(selected_line_start,selected_line_end,'');
-    }
-
-    function highlightComment(comment) {
-	clearHighlighting();
-	highlightLines(comment.line_start,
-		       comment.line_end,
-		       highlightColour);
-	selected_line_start = comment.line_start;
-	selected_line_end = comment.line_end;
-    }
-
     function writeComment(n,comment) {
-	var ob = $('<div class="button">');
-	ob.text(comment.length + " comment(s)");
+	var ob = $('<div>');
+	ob.attr('class','button');
+	var text = comment.length + " comment";
+	if(comment.length > 1)
+	    text += "s";
+	ob.text(text);
 	ob.click(function() {
 	    openComment(n);
 	});
 	$('#comment'+n).append(ob);
     }
-
-    function highlightLines(start,end,colour) {
-	while(start <= end) {
-	    $('#line' + start).css('background',colour);
-	    ++start;
-	}
+    
+/******************************************************************************
+* Code Display                                                                *
+******************************************************************************/
+    
+    function buildCodeTable(n,line) {
+	var toPrint = $('<tr>');
+	toPrint.append($('<td>').append($('<input>')
+					.attr('name','line_start')
+					.attr('type','radio')
+					.val(n)));
+	toPrint.append($('<td>').append($('<input>')
+					.attr('name','line_end')
+					.attr('type','radio')
+					.val(n)));
+	toPrint.append($('<td>').text(n));
+	toPrint.append($('<td>').attr('id','line'+n).attr('class','pre'));
+	toPrint.append($('<td>').attr('id','comment'+n));
+	$('#code_table').append(toPrint);
+	$('td#line'+n).text(line);
     }
 
-    function writeCommentsError() {
-	printComments("An error occured while retrieving comments");
+    function writeCodeLines(code) {
+	if(code === null) return;
+	var lines = code.text.split('\n');
+	for(var i in lines) {
+	    buildCodeTable(Number(i)+1,lines[i]+'\n');
+	}
+	$('input#code_id').val(code.id);
+	getComments(code.id,writeComments,reportError);
     }
     
-    // run when ready
+/******************************************************************************
+* Run when display ready                                                      *
+******************************************************************************/
+
     $(document).ready(function() {
-	comments_div = $('div#comments');
 	var query = URI(document.URL).query(true);
 	if(query.id === undefined) {
-	    printCode('No ID specified');
+	    reportError("Code ID not found");
 	    return;
 	}
-	getCode(query.id,writeCode,writeCodeError);
+	getCode(query.id,writeCodeLines,reportError);
     });
 })();
