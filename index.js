d6e81aeee293b1921f8267a2168fcb661e0a9f2c
Simon Pratt <me@SimonDavidPratt.com>
2012-06-22 17:21:18 -0500

    Added support for languages

--- index.js
+++ index.js
@@ -3,6 +3,26 @@
 * Copyright 2012, Simon Pratt                                                 *
 ******************************************************************************/
 (function() {
+    function reportError(errorText) {
+	$('#error').text(errorText);
+    }
+    
+    function buildLanguageSelect(jqo, data) {
+	var languages = data.languages;
+	if(languages === undefined) {
+	    reportError("Couldn't load languages.");
+	    return;
+	}
+	for(var index in languages) {
+	    var language = languages[index];
+	    console.log('loading '+language.description+'...');
+	    var option = $('<option>');
+	    option.val(language.id);
+	    option.text(language.description);
+	    jqo.append(option);
+	}
+	return;
+    }
 /******************************************************************************
 * Run when display ready                                                      *
 ******************************************************************************/
@@ -10,7 +30,11 @@
 	// display error message, if any
 	var query = URI(document.URL).query(true);
 	if(query.error != undefined) {
-	    $('#error').text(query.error);
+	    reportError(query.error);
 	}
+	console.log('Loading languages....');
+	$.get('/do/languages',function(data) {
+	    buildLanguageSelect($('#language_id'),data);
+	});
     });
 })();
