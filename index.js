15379619b195960f1f9fd1c161bf13d01b4fc5e8
Simon Pratt <me@SimonDavidPratt.com>
2012-07-01 08:14:00 -0500

    Fixed indentation in js and css

--- index.js
+++ index.js
@@ -6,39 +6,41 @@
 * For details about the license, see the LICENSE file.                        *
 ******************************************************************************/
 (function() {
-    function reportError(errorText) {
-	$('#error').text(errorText).show();
-    }
-    
-    function buildLanguageSelect(jqo, data) {
-	var languages = data.languages;
-	if(languages === undefined) {
-	    reportError("Couldn't load languages.");
-	    return;
+	function reportError(errorText) {
+		$('#error').text(errorText).show();
 	}
-	for(var index in languages) {
-	    var language = languages[index];
-	    console.log('loading '+language.description+'...');
-	    var option = $('<option>');
-	    option.val(language.id);
-	    option.text(language.description);
-	    jqo.append(option);
+
+	function buildLanguageSelect(jqo, data) {
+		var languages = data.languages;
+		if(languages === undefined) {
+			reportError("Couldn't load languages.");
+			return;
+		}
+		for(var index in languages) {
+			var language = languages[index];
+			console.log('loading '+language.description+'...');
+			var option = $('<option>');
+			option.val(language.id);
+			option.text(language.description);
+			jqo.append(option);
+		}
+		return;
 	}
-	return;
-    }
+
 /******************************************************************************
 * Run when display ready                                                      *
 ******************************************************************************/
-    $(document).ready(function() {
-	$('#error').hide();
-	// display error message, if any
-	var query = URI(document.URL).query(true);
-	if(query.error != undefined) {
-	    reportError(query.error);
-	}
-	console.log('Loading languages....');
-	$.get('/do/languages',function(data) {
-	    buildLanguageSelect($('#language_id'),data);
+
+	$(document).ready(function() {
+		$('#error').hide();
+		// display error message, if any
+		var query = URI(document.URL).query(true);
+		if(query.error != undefined) {
+			reportError(query.error);
+		}
+		console.log('Loading languages....');
+		$.get('/do/languages',function(data) {
+			buildLanguageSelect($('#language_id'),data);
+		});
 	});
-    });
 })();
